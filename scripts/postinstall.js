95a5e746fd55ecf1b19db299e434641a3a69b48f
稲畑 稔秋 <inahata@miharu.co.jp>
2020-02-24 14:35:43 +0900

    fix

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -3,12 +3,23 @@ const fs = require('fs');
 
 
 const working = process.cwd();
-const current = path.resolve('.');
+const current = path.resolve('./');
+
+console.log(working);
+console.log(current);
+
+const data = {
+  working,
+  current,
+};
+
+const text = JSON.stringify(data);
+
 
 if (working === current) {
   // install to develop
-  fs.writeFileSync('develop', '');
+  fs.writeFileSync('develop', text);
 } else {
   // install to use
-  fs.writeFileSync('use', '');
+  fs.writeFileSync('use', text);
 }
