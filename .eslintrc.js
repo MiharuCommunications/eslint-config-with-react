c095ca16b8a274781202665e6e730b6fcb113ac8
稲畑 稔秋 <inahata@miharu.co.jp>
2020-02-24 10:46:22 +0900

    import

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    "airbnb-typescript",
+  ],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+
+  ignorePatterns: [
+    "node_modules/",
+  ],
+
+  rules: {
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: 'ForInStatement',
+        message: 'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.',
+      },
+      {
+        selector: 'LabeledStatement',
+        message: 'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
+      },
+      {
+        selector: 'WithStatement',
+        message: '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
+      },
+    ],
+    'no-await-in-loop': ['off'],
+
+    // namespace の利用方法が確定するまで保留
+    'no-inner-declarations': ['off'],
+
+    // TypeScript のため不要
+    'react/prop-types': ['off'],
+
+    'no-alert': ['off'],
+    'no-console': ['off'],
+  },
+};
